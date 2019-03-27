2e24017c362fd25b8c78e79b638763f757b03241
Tom Morris <tom@hexbolt.io>
2019-03-26 19:51:01 -0500

    Demonstrated data model binding

--- Pages/Design.cshtml.cs
+++ Pages/Design.cshtml.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace enginethermo.Pages
 {
     public class DesignModel : PageModel
     {
+        
         public string designMessage { get; set; }
+
+        public int simulationMessage {get; set;}
+
+        [BindProperty]
+        public int cycleType {get; set;}
 
         public void OnGet()
         {
             designMessage = "Hello from design model";
+            cycleType = 2;
+        }
+
+        public void OnPostRunSimulation()
+        {
+            simulationMessage = cycleType*2;
+            
+
         }
     }
 }
